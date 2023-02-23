53561cf56ec25adffb94d60ba807a77e9990f554
drunk <550011169@qq.com>
2023-02-23 23:56:05 +0800

    服务优雅退出

--- advance/ctx/graceful_shutdown/main.go
+++ advance/ctx/graceful_shutdown/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"context"
+	"go-demo/advance/ctx/graceful_shutdown/service"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	s1 := service.NewServer("business", "127.0.0.1:8080")
+	s1.Handle("/", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("hello"))
+	}))
+	s2 := service.NewServer("admin", "localhost:8081")
+	app := service.NewApp([]*service.Server{s1, s2}, service.WithShutdownCallbacks(StoreCacheToDBCallBack))
+	app.StartAndServe()
+}
+
+func StoreCacheToDBCallBack(ctx context.Context) {
+	done := make(chan struct{}, 1)
+	go func() {
+		// 你的业务逻辑，比如说这里我们模拟的是将本地缓存刷新到数据库里面
+		// 这里我们简单的睡一段时间来模拟
+		log.Printf("刷新缓存中……")
+		time.Sleep(1 * time.Second)
+	}()
+	select {
+	case <-ctx.Done():
+		log.Println("刷新缓存超时")
+	case <-done:
+		log.Println("缓存被刷新到了 DB")
+	}
 }
--- advance/ctx/graceful_shutdown/service/shutdown.go
+++ advance/ctx/graceful_shutdown/service/shutdown.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -12,11 +15,13 @@ type Option func(*App)
 type ShutdownCallback func(ctx context.Context)
 
 func WithShutdownCallbacks(cbs ...ShutdownCallback) Option {
-	panic("implement me")
+	return func(app *App) {
+		app.cbs = cbs
+	}
 }
 
 type App struct {
-	servers []*http.Server
+	servers []*Server
 
 	shutdownTimeout time.Duration
 
@@ -24,6 +29,19 @@ type App struct {
 	cbTimeout time.Duration
 
 	cbs []ShutdownCallback
+}
+
+func NewApp(servers []*Servers, opts ...Option) *App {
+	res := &App{
+		servers:         servers,
+		waitTime:        10 * time.Second,
+		cbTimeout:       3 * time.Second,
+		shutdownTimeout: 30 * time.Second,
+	}
+	for _, opt := range opts {
+		opt(res)
+	}
+	return res
 }
 
 type Server struct {
@@ -51,8 +69,84 @@ func (app *App) StartAndServe() {
 	for _, s := range app.servers {
 		srv := s
 		go func() {
-			if err := srv.Star
+			if err := srv.Start(); err != nil {
+				if err == http.ErrServerClosed {
+					log.Printf("服务器%s已关闭", srv.name)
+				} else {
+					log.Printf("服务器%s异常退出", srv.name)
+				}
+			}
 		}()
+	}
+
+	ch := make(chan os.Signal, 2)
+	signal.Notify(ch, signals...)
+	go func() {
+		select {
+		case <-ch:
+			log.Printf("强制退出")
+			os.Exit(1)
+		case <-time.After(app.shutdownTimeout):
+			log.Printf("超时强制退出")
+			os.Exit(1)
+		}
+	}()
+	app.shutdown()
+}
+
+func (app *App) shutdown() {
+	log.Println("开始关闭应用，停止接收新请求")
+	for _, s := range app.servers {
+		s.rejectReq()
+	}
+	log.Println("等待正在执行请求完毕")
+	time.Sleep(app.waitTime)
+	log.Println("开始关闭服务器")
+
+	var wg sync.WaitGroup
+	wg.Add(len(app.servers))
+	for _, srv := range app.servers {
+		srvCp := srv
+		go func() {
+			if err := srvCp.stop(); err != nil {
+				log.Println("关闭服务失败", srvCp.name)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	log.Println("开始执行自定义回调")
+	wg.Add(len(app.cbs))
+	for _, cb := range app.cbs {
+		c := cb
+		go func() {
+			ctx, cancel := context.WithTimeout(context.Background(), app.cbTimeout)
+			c(ctx)
+			cancel()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	log.Println("开始释放资源")
+	app.close()
+}
+
+func (app *App) close() {
+	// 在这里释放掉一些可能的资源
+	time.Sleep(time.Second)
+	log.Println("应用关闭")
+}
+
+func NewServer(name string, addr string) *Server {
+	mux := &serverMux{ServeMux: http.NewServeMux()}
+	return &Server{
+		name: name,
+		mux:  mux,
+		srv: &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		},
 	}
 }
 
@@ -68,8 +162,7 @@ func (s *Server) rejectReq() {
 	s.mux.reject = true
 }
 
-func (s *Server) stop(ctx context.Context) error {
+func (s *Server) stop() error {
 	log.Printf("服务器%s关闭中", s.name)
-	return s.srv.Shutdown(ctx)
+	return s.srv.Shutdown(context.Background())
 }
-
